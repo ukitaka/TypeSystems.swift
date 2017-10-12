f0c780a708f52f1fb4ccf9bf0b6c0f865a2c9afd
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-12 18:18:34 +0900

    Add useful operators

---
+++ Sources/Utils/Operators.swift
@@ -0,0 +1,5 @@
+//
+// Created by ukitaka on 2017/10/12.
+//
+
+infix operator ∈
--- Sources/Utils/Set+Extension.swift
+++ Sources/Utils/Set+Extension.swift
@@ -6,4 +6,12 @@ public extension Set {
     public func toArray() -> Array<Element> {
         return map { $0 }
     }
+
+    public static func /<T>(lhs: Set<T>, rhs: Set<T>) -> Set<T> {
+        return lhs.subtracting(rhs)
+    }
+
+    public static func ∈<T>(lhs: T, rhs: Set<T>) -> Bool {
+        return rhs.contains(lhs)
+    }
 }
