18ff275c843c8f59cf8d3555a21f5cc8242ea432
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-12 23:42:27 +0900

    Add operators

--- Sources/Utils/Operators.swift
+++ Sources/Utils/Operators.swift
@@ -2,7 +2,22 @@
 // Created by ukitaka on 2017/10/12.
 //
 
+precedencegroup CompositionPrecedence {
+    associativity: right
+    higherThan: BitwiseShiftPrecedence
+}
+
+precedencegroup PrecedenceLeft {
+    associativity: left
+}
+
 infix operator ∈
 infix operator ∋
 infix operator ∉
 infix operator ∌
+infix operator ↦
+infix operator →
+infix operator • : CompositionPrecedence
+infix operator ⊢
+infix operator ∪: PrecedenceLeft
+infix operator ∩: PrecedenceLeft
