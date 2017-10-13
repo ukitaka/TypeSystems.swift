bf7ea1871e6459f3f50a73dadaf96a1bf47807dd
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-13 22:18:55 +0900

    Add `Parsec`

--- Package.swift
+++ Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 let package = Package(
     name: "TypeSystems",
     products: [
+      .library(name: "Parsec", targets: ["Parsec"]),
       .library(name: "AlgorithmW", targets: ["AlgorithmW"]),
       .library(name: "Utils", targets: ["Utils"])
     ],
     targets: [
+      .target(name: "Parsec", dependencies: []),
       .target(name: "AlgorithmW", dependencies: ["Utils"]),
       .target(name: "Utils", dependencies: []),
       .testTarget(name: "AlgorithmWTests", dependencies: ["AlgorithmW"]), 
