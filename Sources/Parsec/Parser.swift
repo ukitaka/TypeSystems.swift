e25ee450810762f6fe8279892e691cfb39cf94d3
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-13 22:45:24 +0900

    Add `map` and `flatMap`

--- Sources/Parsec/Parser.swift
+++ Sources/Parsec/Parser.swift
@@ -8,4 +8,18 @@ public struct Parser<A> {
     public init(parse: @escaping Parse<A>) {
         self.parse = parse
     }
+
+    public func map<B>(_ f: @escaping (A) -> B) -> Parser<B> {
+        return Parser<B> { string in
+            let (a, remaining) = self.parse(string)
+            return (f(a), remaining)
+        }
+    }
+
+    public func flatMap<B>(_ f: @escaping (A) -> Parser<B>) -> Parser<B> {
+        return Parser<B> { string in
+            let (a, remaining) = self.parse(string)
+            return f(a).parse(remaining)
+        }
+    }
 }
