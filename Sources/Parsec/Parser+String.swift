5be46164a45d6737afcd913ac551e4822f766f69
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-13 23:35:26 +0900

    Add `Parser.string`

---
+++ Sources/Parsec/Parser+String.swift
@@ -0,0 +1,11 @@
+//
+// Created by ukitaka on 2017/10/13.
+//
+
+extension Parser {
+    static func string(_ str: String) -> Parser<String> {
+        return Parser<String> { input in
+            return ("", "") //TODO:
+        }
+    }
+}
