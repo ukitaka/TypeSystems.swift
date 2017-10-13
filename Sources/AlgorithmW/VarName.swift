1f0684adb5827a9aab9fb38398517dc34a08bfa4
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-13 16:09:04 +0900

    Add MARK comments

--- Sources/AlgorithmW/VarName.swift
+++ Sources/AlgorithmW/VarName.swift
@@ -6,17 +6,23 @@ public struct VarName {
     public let name: String
 }
 
+// MARK: - Equatable
+
 extension VarName: Equatable {
     public static func ==(lhs: VarName, rhs: VarName) -> Bool {
         return lhs.name == rhs.name
     }
 }
 
+// MARK: - Hashable
+
 extension VarName: Hashable {
     public var hashValue: Int {
         return name.hashValue
     }
 }
+
+// MARK: - ExpressibleByStringLiteral
 
 extension VarName: ExpressibleByStringLiteral {
     public init(stringLiteral value: StringLiteralType) {
