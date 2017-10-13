68de10ad25b2aa6197c6d239335757cc84ec707a
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-13 16:04:20 +0900

    Implement `Equatable`, `Hashable`, `ExpressibleByStringLiteral`

--- Sources/AlgorithmW/VarName.swift
+++ Sources/AlgorithmW/VarName.swift
@@ -5,3 +5,25 @@
 public struct VarName {
     public let name: String
 }
+
+extension VarName: Equatable {
+    public static func ==(lhs: VarName, rhs: VarName) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
+extension VarName: Hashable {
+    public var hashValue: Int {
+        return name.hashValue
+    }
+}
+
+extension VarName: ExpressibleByStringLiteral {
+    public init(stringLiteral value: StringLiteralType) {
+        self.name = value
+    }
+
+    public init(unicodeScalarLiteral value: ExtendedGraphemeClusterLiteralType) {
+        self.name = value
+    }
+}
