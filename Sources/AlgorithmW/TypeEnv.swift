b47ecc17ff60a9d3922e4f1cf4c03994b3797a8e
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-13 17:15:13 +0900

    Add `TypeEnv`

---
+++ Sources/AlgorithmW/TypeEnv.swift
@@ -0,0 +1,31 @@
+//
+// Created by ukitaka on 2017/10/13.
+//
+
+public struct TypeEnv {
+    private let env: [VarName: Type]
+
+    public init() {
+        self.env = [:]
+    }
+
+    public init(varName: VarName, type: Type) {
+        self.env = [varName: type]
+    }
+
+    public subscript(_ varName: VarName) -> Type? {
+        return env[varName]
+    }
+}
+
+// MARK: alias of `init`
+
+public extension TypeEnv {
+    public static func empty() -> TypeEnv {
+        return TypeEnv()
+    }
+
+    public static func singleton(varName: VarName, type: TypeEnv) -> TypeEnv {
+        return TypeEnv()
+    }
+}
