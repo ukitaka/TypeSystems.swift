6c0c7dae7b52e8f315870e9cbd988f52b9f5114f
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-14 09:31:48 +0900

    Add `Parsec` to dependencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
       .target(name: "Parsec", dependencies: []),
-      .target(name: "AlgorithmW", dependencies: ["Utils"]),
+      .target(name: "AlgorithmW", dependencies: ["Parsec", "Utils"]),
       .target(name: "Utils", dependencies: []),
       .testTarget(name: "AlgorithmWTests", dependencies: ["AlgorithmW"]), 
       .testTarget(name: "UtilsTests", dependencies: ["Utils"]),
