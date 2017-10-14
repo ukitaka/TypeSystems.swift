06e8c712afc42063fa46dc481a591a91ff274929
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-14 10:38:01 +0900

    Add `char` to `Parser`

--- Sources/Parsec/Parser+String.swift
+++ Sources/Parsec/Parser+String.swift
@@ -3,6 +3,16 @@
 //
 
 public extension Parser {
+    public static func char(_ char: Character) -> Parser<Character> {
+        return Parser<Character> { input in
+            if input.characters.first == char {
+                return .success(char, input.removingFirst())
+            } else {
+                return .failure(.error("input doesn't has prefix character'\(char)'"))
+            }
+        }
+    }
+
     public static func string(_ str: String) -> Parser<String> {
         return Parser<String> { input in
             if input.hasPrefix(str) {
--- Sources/Parsec/String+Extension.swift
+++ Sources/Parsec/String+Extension.swift
@@ -3,6 +3,12 @@
 //
 
 extension String {
+    func removingFirst() -> String {
+        var str = self
+        str.removeFirst()
+        return str
+    }
+
     func removing(head: String) -> String {
         var str = self
         str.removeFirst(head.characters.count)
