a407e5c9706563ff74dbb15dbe35d783e55c3a62
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-14 10:25:48 +0900

    Add `Parser` to `AlgorithmW`

---
+++ Sources/AlgorithmW/Parser.swift
@@ -0,0 +1,21 @@
+//
+// Created by ukitaka on 2017/10/14.
+//
+
+import Parsec
+
+public struct Parser {
+    public func parse(_ input: String) -> ParseResult<Term> {
+        return .failure(.error("not implemented yet"))
+    }
+}
+
+// MARK: - internals
+
+extension Parser {
+    typealias P<A> = Parsec.Parser<A>
+
+    func parseIntLiteral(_ input: String) -> P<Term> {
+        fatalError()
+    }
+}
