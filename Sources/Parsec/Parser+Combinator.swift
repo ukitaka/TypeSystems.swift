73c0375f1e0e937844ea712ee3b5ad84489f2f15
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-14 11:19:22 +0900

    Add `listOf` combinator

--- Sources/Parsec/Parser+Combinator.swift
+++ Sources/Parsec/Parser+Combinator.swift
@@ -2,6 +2,8 @@
 // Created by ukitaka on 2017/10/14.
 //
 
+// MARK: - or
+
 public extension Parser {
     public func or(_ other: @autoclosure @escaping () -> Parser<A>) -> Parser<A> {
         return Parser<A> { input in
@@ -21,5 +23,21 @@ public extension Parser {
 
     public static func <|> (lhs: Parser<A>, rhs: @autoclosure @escaping () -> Parser<A>) -> Parser<A> {
         return lhs.or(rhs)
+    }
+}
+
+// MARK: - many
+
+public extension Parser {
+    public func listOf(_ n: UInt) -> Parser<[A]> {
+        if n == 0 {
+            return Parser<[A]>.success([])
+        } else {
+            return self.map2(self.listOf(n - 1)) { (a, list) in
+                var list = list
+                list.insert(a, at: 0)
+                return list
+            }
+        }
     }
 }
