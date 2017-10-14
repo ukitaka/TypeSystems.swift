46b03cd927147664aa950a7f36725abf4e9ac3bd
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-14 10:48:20 +0900

    Add `or`

--- Sources/Parsec/Parser+Combinator.swift
+++ Sources/Parsec/Parser+Combinator.swift
@@ -3,4 +3,19 @@
 //
 
 public extension Parser {
+    func or(_ other: @autoclosure @escaping () -> Parser<A>) -> Parser<A> {
+        return Parser<A> { input in
+            switch self.parse(input) {
+            case let .success(a, remaining):
+                return .success(a, remaining)
+            case .failure:
+                switch other().parse(input) {
+                case let .success(a, remaining):
+                    return .success(a, remaining)
+                case .failure:
+                    return .failure(.error("or error"))
+                }
+            }
+        }
+    }
 }
