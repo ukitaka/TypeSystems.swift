f8c7396600e172ad0ef3db188c2d28129de7fd97
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-19 17:33:49 +0900

    Add `typeInference` functions

--- Sources/AlgorithmW/TypeInference.swift
+++ Sources/AlgorithmW/TypeInference.swift
@@ -2,3 +2,16 @@
 // Created by ukitaka on 2017/10/13.
 //
 
+
+func typeInferenceliteral(env: TypeEnv, literal: Literal) -> (Substitution, Type) {
+    fatalError()
+}
+
+func typeInferenceTerm(env: TypeEnv, term: Term) -> (Substitution, Type) {
+    fatalError()
+}
+
+public func typeInference(env: TypeEnv, term: Term) -> Type {
+    let (substitution, type) = typeInferenceTerm(env: env, term: term)
+    return substitution.apply(to: type)
+}
