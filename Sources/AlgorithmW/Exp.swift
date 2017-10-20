440b80cbdf17ca2f2592a209a2815575e0f1429e
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-20 22:45:40 +0900

    Add `if` and `fix`

--- Sources/AlgorithmW/Exp.swift
+++ Sources/AlgorithmW/Exp.swift
@@ -7,9 +7,11 @@ import Utils
 public indirect enum Exp {
     case `var`(VarName)
     case literal(Literal)
+    case `if`(Exp, Exp, Exp)
     case abs(VarName, Exp)
     case app(Exp, Exp)
     case `let`(VarName, Exp, Exp)
+    case fix(VarName, Exp)
 }
 
 // MARK: - Equatable
@@ -21,35 +23,18 @@ extension Exp: Equatable {
             return v1 == v2
         case let (.literal(l1), .literal(l2)):
             return l1 == l2
+        case let (.if(c1, t1, e1), .if(c2, t2, e2)):
+            return c1 == c2 && t1 == t2 && e1 == e2
         case let (.abs(v1, t1), .abs(v2, t2)):
             return v1 == v2 && t1 == t2
         case let (.app(tl1, tl2), .app(tr1, tr2)):
             return tl1 == tr1 && tl2 == tr2
         case let (.let(n1, bind1, body1), .let(n2, bind2, body2)):
             return n1 == n2 && bind1 == bind2 && body1 == body2
+        case let (.fix(n1, e1), .fix(n2, e2)):
+            return n1 == n2 && e1 == e2
         default:
             return false
-        }
-    }
-}
-
-// MARK: - Free variables
-//
-// 3.5 Type assignments
-//
-public extension Exp {
-    var freeVars: Set<VarName> {
-        switch self {
-        case let .var(varName):
-            return Set.singleton(varName)
-        case .literal:
-            return Set.empty()
-        case let .abs(varName, term):
-            return term.freeVars / Set.singleton(varName)
-        case let .app(fun, arg):
-            return fun.freeVars ∪ arg.freeVars
-        case let .let(varName, bind, body):
-            return bind.freeVars ∪ (body.freeVars / Set.singleton(varName))
         }
     }
 }
