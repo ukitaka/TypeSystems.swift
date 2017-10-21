1faa1eff6210b446c53933204882af78da4c6a9f
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-21 18:11:46 +0900

    Add test for `if`

--- Tests/AlgorithmWTests/AlgorithmWTests.swift
+++ Tests/AlgorithmWTests/AlgorithmWTests.swift
@@ -5,6 +5,11 @@ class AlgorithmWTests: XCTestCase {
     func testSample() {
         XCTAssertEqual(AlgorithmW.w(.empty(), .literal(.int(1))).1.type, .int)
         XCTAssertEqual(AlgorithmW.w(.empty(), .literal(.bool(true))).1.type, .bool)
+
+        let tru = Exp.literal(.bool(true))
+        let one = Exp.literal(.int(1))
+        let two = Exp.literal(.int(2))
+        XCTAssertEqual(AlgorithmW.w(.empty(), .if(tru, one, two)).1.type, .int)
     }
 }
 
