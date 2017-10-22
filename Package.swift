03419751da935f4aa66b4d968cdcb65b757f40ac
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-23 00:05:14 +0900

    Add `Fullsub`

--- Package.swift
+++ Package.swift
@@ -7,14 +7,17 @@ let package = Package(
     products: [
       .library(name: "Parsec", targets: ["Parsec"]),
       .library(name: "AlgorithmW", targets: ["AlgorithmW"]),
+      .library(name: "Fullsub", targets: ["Fullsub"]),
       .library(name: "Utils", targets: ["Utils"])
     ],
     targets: [
       .target(name: "Parsec", dependencies: ["Utils"]),
       .target(name: "AlgorithmW", dependencies: ["Parsec", "Utils"]),
+      .target(name: "Fullsub", dependencies: ["Parsec", "Utils"]),
       .target(name: "Utils", dependencies: []),
       .testTarget(name: "ParsecTests", dependencies: ["Parsec"]), 
       .testTarget(name: "AlgorithmWTests", dependencies: ["AlgorithmW"]), 
+      .testTarget(name: "FullsubTests", dependencies: ["Fullsub"]), 
       .testTarget(name: "UtilsTests", dependencies: ["Utils"]),
     ]
 )
---
+++ Tests/FullsubTests/FullsubTests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import Fullsub
+
+class FullsubTests: XCTestCase {
+    func testSample() { }
+}
