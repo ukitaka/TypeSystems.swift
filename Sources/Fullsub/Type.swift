6316ab5a182a4ed988cdeaf6192c3be8d99656dc
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-24 22:26:28 +0900

    Add `isSubType`

--- Sources/Fullsub/Type.swift
+++ Sources/Fullsub/Type.swift
@@ -5,7 +5,7 @@
 public indirect enum Type {
     case top
     case `func`(Type, Type)
-    case record([Type])
+    case record([Label: Type])
 }
 
 // MARK: - Equatable
@@ -21,6 +21,30 @@ extension Type: Equatable {
             return types1 == types2
         default:
             return false
+        }
+    }
+}
+
+// MARK: - subtype
+
+public extension Type {
+    public func isSubType(of type: Type) -> Bool {
+        switch (self, type) {
+        case (_, .top):
+            return true
+        case let (.func(argSub, retSub), .func(argSup, retSup)):
+            return argSup.isSubType(of: argSub) && retSub.isSubType(of: retSup)
+        case let (.record(typesSub), .record(typesSup)):
+            return typesSup.reduce(true) { (isSubType, tuple: (Label, Type)) -> Bool in
+                let (label, superType) = tuple
+                if let subType = typesSub[label] {
+                    return isSubType && superType.isSubType(of: subType)
+                }
+                return true
+            }
+        default:
+            return false
+
         }
     }
 }
--- Tests/FullsubTests/FullsubTests.swift
+++ Tests/FullsubTests/FullsubTests.swift
@@ -2,5 +2,20 @@ import XCTest
 @testable import Fullsub
 
 class FullsubTests: XCTestCase {
-    func testSample() { }
+    func testIsSubType() {
+        let recordType1 = Type.record([
+            "hoge": Type.top,
+            "fuga": Type.top
+        ])
+
+        let recordType2 = Type.record([
+            "hoge": Type.top,
+            "fuga": Type.top,
+            "piyo": Type.top
+        ])
+
+        XCTAssertTrue(recordType2.isSubType(of: recordType1))
+        XCTAssertTrue(recordType1.isSubType(of: .top))
+        XCTAssertTrue(recordType2.isSubType(of: .top))
+    }
 }
