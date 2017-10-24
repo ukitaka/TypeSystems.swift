ec492dde8e4d1b327fb6880aa3dc79a0fc86d026
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-24 21:45:31 +0900

    Make it Equatable

--- Sources/Fullsub/Term.swift
+++ Sources/Fullsub/Term.swift
@@ -8,5 +8,26 @@ public indirect enum Term {
     case abs(VarName, Type, Term)
     case app(Term, Term)
     case record([Label: Term])
-    case proj(Label, Label)
+    case proj(Term, Label)
+}
+
+// MARK: -
+
+extension Term: Equatable {
+    public static func ==(lhs: Term, rhs: Term) -> Bool {
+        switch(lhs, rhs) {
+        case let (.var(name1), .var(name2)):
+            return name1 == name2
+        case let (.abs(name1, type1, term1), .abs(name2, type2, term2)):
+            return name1 == name2 && type1 == type2 && term1 == term2
+        case let (.app(fun1, arg1), .app(fun2, arg2)):
+            return fun1 == fun2 && arg1 == arg2
+        case let (.record(elements1), .record(elements2)):
+            return elements1 == elements2
+        case let (.proj(term1, label1), .proj(term2, label2)):
+            return term1 == term2 && label1 == label2
+        default:
+            return false
+        }
+    }
 }
--- Sources/Fullsub/Type.swift
+++ Sources/Fullsub/Type.swift
@@ -5,4 +5,22 @@
 public indirect enum Type {
     case top
     case `func`(Type, Type)
+    case record([Type])
+}
+
+// MARK: - Equatable
+
+extension Type: Equatable {
+    public static func ==(lhs: Type, rhs: Type) -> Bool {
+        switch (lhs, rhs) {
+        case (.top, .top):
+            return true
+        case let (.func(arg1, ret1), .func(arg2, ret2)):
+            return arg1 == arg2 && ret1 == ret2
+        case let (.record(types1), .record(types2)):
+            return types1 == types2
+        default:
+            return false
+        }
+    }
 }
