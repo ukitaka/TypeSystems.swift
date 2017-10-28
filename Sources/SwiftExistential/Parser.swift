815037d9f262861bd6f03a94592f6481a5d1e7ef
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-28 15:06:09 +0900

    Add `parseProtocolDecl`

--- Sources/SwiftExistential/Parser.swift
+++ Sources/SwiftExistential/Parser.swift
@@ -2,8 +2,10 @@
 // Created by ukitaka on 2017/10/28.
 //
 
-import Foundation
+import Parsec
 
 public struct Parser {
-
+    public func parseProtocolDecl(_ input: String) throws -> Term {
+        fatalError()
+    }
 }
