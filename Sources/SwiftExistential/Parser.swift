0b5014a532c1ecc70779651dfc2fae14bb34a896
ukitaka <yuki.takahashi.1126@gmail.com>
2017-10-28 16:49:16 +0900

    Add `methodSignatureParser`

--- Sources/SwiftExistential/Parser.swift
+++ Sources/SwiftExistential/Parser.swift
@@ -12,6 +12,38 @@ public struct Parser {
         return Parsers.alphabets()
     }
 
+    func typeParser() -> P<Type> {
+        return (Parsers.string("Int")
+                <|> Parsers.string("Void")
+                <|> Parsers.string("Bool")
+                <|> self.nameParser()).map(Type.var)
+    }
+
+    func methodSignatureParser() -> P<Term.Method> {
+        let p = Parsers.string("func")
+                *> Parsers.whiteSpaces()
+                *> self.nameParser()
+                <* Parsers.whiteSpaces()
+                <* Parsers.string("(")
+                <* Parsers.whiteSpaces()
+                <* Parsers.string("arg")
+                <* Parsers.whiteSpaces()
+                <* Parsers.string(":")
+                <**> self.typeParser()
+                <* Parsers.whiteSpaces()
+                <* Parsers.string(")")
+                <* Parsers.whiteSpaces()
+                <* Parsers.string("->")
+                <* Parsers.whiteSpaces()
+                <**> self.typeParser()
+        return p.map { result in
+            let name: String = result.0.0
+            let arg: Type = result.0.1
+            let ret: Type = result.1
+            return Term.Method(name: name, type: .func(arg, ret))
+        }
+    }
+
     func protocolDeclParser() -> P<ProtocolDecl> {
         fatalError()
 //        return Parsers.string("protocol")
